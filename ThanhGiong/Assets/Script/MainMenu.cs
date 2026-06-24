c63109fb76a863c16d0e1808db9b291f6b238153
qoochng <unghung102@gmail.com>
2026-06-24 07:44:59 +0700

    Inited commit

--- ThanhGiong/Assets/Script/MainMenu.cs
+++ ThanhGiong/Assets/Script/MainMenu.cs
@@ -18,11 +18,9 @@ public class MainMenu : MonoBehaviour
 
     private IEnumerator FadeAndLoadScene()
     {
-        // 1. Bật chặn chuột để người chơi không bấm đúp loạn xạ trong lúc đang chuyển cảnh
         // 1. Khóa chuột không cho người chơi bấm linh tinh
         fadeGroup.blocksRaycasts = true;
 
-        // 2. Tăng dần độ mờ (Alpha) từ 0 lên 1
         // 2. GỌI LỆNH TẮT NHẠC (thời gian tắt nhạc bằng đúng thời gian đen màn hình)
         if (PersistentBGM.instance != null)
         {
@@ -35,14 +33,11 @@ public class MainMenu : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             fadeGroup.alpha = elapsedTime / fadeDuration;
-            yield return null; // Đợi frame tiếp theo để tạo độ mượt
             yield return null;
         }
 
-        // Đảm bảo màn hình đen kịt 100%
         fadeGroup.alpha = 1f;
 
-        // 3. Chuyển Scene sau khi màn hình đã đen
         // 4. Chuyển sang màn chơi Game
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
